f727715bebcb95be086dddf09e0515d3c52ba6b4
ctaylor4874 <ctaylor4874@gmail.com>
2017-01-08 23:47:13 -0600

    Used json to pull location data to pass into getURL() function.  Program now gets user location, locates restaurants near them, and gets all website url's for those websites.

--- __init__.py
+++ __init__.py
@@ -8,6 +8,8 @@ import os
 from flask import Flask, render_template, request, redirect, url_for, session, Markup
 import sys
 from user import *
+import json
+import requests
 
 reload(sys)
 sys.setdefaultencoding('utf8')
@@ -16,8 +18,25 @@ app = Flask(__name__)
 
 @app.route('/')
 def getMap():
-    # user = User()
     return render_template('location.html', apikey = config.apikey)
+
+@app.route('/_location')
+def location():
+    user = User()
+    lat = json.loads(request.args.get('lat'))
+    lng = json.loads(request.args.get('lng'))
+    # do some stuff
+    user.loc = {'latitude':lat, 'longitude':lng}
+    user.getURL(user.loc)
+    return render_template('location.html', apikey=config.apikey)
+
+
+    # response = request.get_json('loc')
+    # loc = json.loads(response.text)
+    # # do some stuff
+    # print (loc[0])
+
+
 
 if __name__ == "__main__":
     app.run()
--- user.py
+++ user.py
@@ -11,53 +11,60 @@ apikey = config.apikey
 
 
 # gets restaurants from a given location
-# class User:
-#     def getPlaces(location):
-#         url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=%s&rankby=distance&type=restaurant&key=%s" % (
-#             urllib.quote_plus(location), urllib.quote_plus(apikey))
-#         response = StringIO.StringIO()
-#         c = pycurl.Curl()
-#         c.setopt(c.URL, url)
-#         c.setopt(c.WRITEFUNCTION, response.write)
-#         c.setopt(c.HTTPHEADER, ['Content-Type: application/json', 'Accept-Charset: UTF-8'])
-#         c.setopt(c.POSTFIELDS, '@request.json')
-#         c.perform()
-#         c.close()
-#         places = json.loads(response.getvalue())
-#         response.close()
-#         return places
-#
-#
-#     # sample location, Heights Houston
-#     # getPlaces('29.799592,-95.420138')
-#     #
-#     # gets additional info on the given place_id
-#     def getInfo(place_id):
-#         url = "https://maps.googleapis.com/maps/api/place/details/json?placeid=%s&key=%s" % (
-#             urllib.quote_plus(place_id), urllib.quote_plus(apikey))
-#         response = StringIO.StringIO()
-#         c = pycurl.Curl()
-#         c.setopt(c.URL, url)
-#         c.setopt(c.WRITEFUNCTION, response.write)
-#         c.setopt(c.HTTPHEADER, ['Content-Type: application/json', 'Accept-Charset: UTF-8'])
-#         c.setopt(c.POSTFIELDS, '@request.json')
-#         c.perform()
-#         c.close()
-#         info = json.loads(response.getvalue())
-#         response.close()
-#         return info
-#
-#
-#     # takes in location and pulls data from getPlaces and getInfo
-#     location = sys.argv[1]
-#     places = getPlaces(location)
-#     tmp = places.get('results')
-#     for place in tmp:
-#         # stores place_id from query
-#         place_id = [place][0].get('place_id')
-#         # runs query to get additional info from the place_id
-#         info = getInfo(place_id)
-#         # store website url
-#         tmp_web = info.get('result').get('website')
-#
-#         # todo scrape tmp_web sites for keywords
+class User:
+    def __init__(self):
+        self.location = {}
+        self.place_id = ''
+        self.loc = {}
+
+    def getPlaces(self):
+        url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=%s&rankby=distance&type=restaurant&key=%s" % (
+            urllib.quote_plus(self.location), urllib.quote_plus(apikey))
+        response = StringIO.StringIO()
+        c = pycurl.Curl()
+        c.setopt(c.URL, url)
+        c.setopt(c.WRITEFUNCTION, response.write)
+        c.setopt(c.HTTPHEADER, ['Content-Type: application/json', 'Accept-Charset: UTF-8'])
+        c.setopt(c.POSTFIELDS, '@request.json')
+        c.perform()
+        c.close()
+        self.places = json.loads(response.getvalue())
+        response.close()
+        return self.places
+
+    # sample location, Heights Houston
+    # getPlaces('29.799592,-95.420138')
+    #
+    # gets additional info on the given place_id
+    def getInfo(self):
+        url = "https://maps.googleapis.com/maps/api/place/details/json?placeid=%s&key=%s" % (
+            urllib.quote_plus(self.place_id), urllib.quote_plus(apikey))
+        response = StringIO.StringIO()
+        c = pycurl.Curl()
+        c.setopt(c.URL, url)
+        c.setopt(c.WRITEFUNCTION, response.write)
+        c.setopt(c.HTTPHEADER, ['Content-Type: application/json', 'Accept-Charset: UTF-8'])
+        c.setopt(c.POSTFIELDS, '@request.json')
+        c.perform()
+        c.close()
+        self.info = json.loads(response.getvalue())
+        response.close()
+        return self.info
+
+
+# takes in location and pulls data from getPlaces and getInfo
+    def getURL(self, loc):
+        self.location = str(loc['latitude']) + ',' + str(loc['longitude'])
+        print self.location
+        places = User.getPlaces(self)
+        tmp = places.get('results')
+        for place in tmp:
+            # stores place_id from query
+            self.place_id = [place][0].get('place_id')
+            # runs query to get additional info from the place_id
+            info = User.getInfo(self)
+            # store website url
+            tmp_web = info.get('result').get('website')
+            print tmp_web
+
+        # todo scrape tmp_web sites for keywords

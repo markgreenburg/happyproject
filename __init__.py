2a3ef7536763260c95e2c48152a02619536e59fa
ctaylor4874 <ctaylor4874@gmail.com>
2017-01-25 20:39:51 -0600

    fixed app not running

--- __init__.py
+++ __init__.py
@@ -202,7 +202,7 @@ def submit_page_edit():
     pass
 
 if __name__ == "__main__":
-    app.run(threaded=True)
+    # app.run(threaded=True)
     app.secret_key = config.SECRET_KEY
     # app.config['APPLICATION_ROOT'] = config.APPLICATION_ROOT
     app.config['DEBUG'] = config.DEBUG
